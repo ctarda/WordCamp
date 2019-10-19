acbe9cfc6a0c8708e7531e5cee33d9bd3d9e749e
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-10-19 18:13:06 +0800

    Add WatchEventViewModel

---
+++ WordCampsWatch Extension/ViewModel/WatchEventViewModel.swift
@@ -0,0 +1,55 @@
+import Foundation
+import Combine
+
+final class WatchEventViewModel: ObservableObject, Identifiable {
+    private let event: WordCamp
+    private var cancellabe: AnyCancellable?
+
+    var objectWillChange = PassthroughSubject<WatchEventViewModel, Never>()
+
+    var id: Int {
+        return event.id
+    }
+
+    var readableTitle: String {
+        return event.title.rendered
+    }
+
+    var readableContent: String {
+        return event.content.rendered.strippedHTML
+    }
+
+    var location: String {
+        return event.location
+    }
+
+    var startDate: String {
+        return event.startDate.toString(dateStyle: .medium, timeStyle: .none)
+    }
+
+    var url: String {
+        return event.url
+    }
+
+    var venueName: String {
+        return event.venueName
+    }
+
+    var venueAddress: String {
+        return event.venueAddress
+    }
+
+    private var mediaID: Int {
+        return event.featuredMedia
+    }
+
+    init(event: WordCamp) {
+        self.event = event
+    }
+
+    func matches(textFilter: String) -> Bool {
+        return readableTitle
+            .lowercased()
+            .contains(textFilter.lowercased())
+    }
+}
