7f4dbb38c6075110eb9cd3262a8277fd5501d1f0
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-10-19 17:53:35 +0800

    Add watch app target

---
+++ WordCampsWatch Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WordCampsWatch Extension
+//
+//  Created by Cesar Tardaguila on 19/10/2019.
+//  Copyright © 2019 automattic. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WordCampsWatch Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  WordCampsWatch Extension
+//
+//  Created by Cesar Tardaguila on 19/10/2019.
+//  Copyright © 2019 automattic. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
