32d087050aebd1b1b0c7250cb65760512a5a9dc0
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-10-27 16:47:10 +0800

    Improve annotation

--- Shared code/ViewModel/WordCampAnnotation.swift
+++ Shared code/ViewModel/WordCampAnnotation.swift
@@ -2,10 +2,12 @@ import MapKit
 
 final class WordCampAnnotation: NSObject, MKAnnotation {
     let coordinate: CLLocationCoordinate2D
-    let title: NSString
+    let title: String?
+    let subtitle: String?
 
     init(event: WordCamp) {
         self.coordinate = event.venueCoordinates.locationCoordinates
-        self.title = NSString(string: event.title.rendered)
+        self.title = event.location
+        self.subtitle = event.venueName
     }
 }
