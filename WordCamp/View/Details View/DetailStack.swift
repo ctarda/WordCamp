14673ee33b75c19ae99304e1a764a3f32fc921df
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-12-10 11:57:25 +0800

    Add scrollview and some color to the dates

--- WordCamp/View/Details View/DetailStack.swift
+++ WordCamp/View/Details View/DetailStack.swift
@@ -4,22 +4,35 @@ struct DetailStack: View {
     let event: PhoneEventViewModel
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text(event.readableTitle).font(.title)
-                .lineLimit(2)
-            HStack {
-                Text(event.location).font(.headline)
-                Spacer()
-                Text(event.date).font(.headline)
+        VStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text(event.readableTitle).font(.title)
+                        .lineLimit(2)
+
+                    Divider()
+
+                    HStack {
+                        Text(event.location).font(.headline)
+                        Spacer()
+                        Text(event.date).font(.headline)
+                            .foregroundColor(.secondary)
+                    }
+
+                    Divider()
+
+                    Text(event.venueName).font(.headline)
+                    Text(event.venueAddress).font(.headline)
+
+                    Divider()
+
+                    Text(event.readableContent)
+                        .font(.body)
+                        .lineLimit(Int.max)
+
+                    Spacer()
+                }
             }
-            Text(event.venueName).font(.headline)
-            Text(event.venueAddress).font(.headline)
-            Text(event.readableContent)
-                .font(.body)
-                .lineLimit(Int.max)
-
-            Spacer()
-
             OpenLinkButton(event: event)
         }
         .padding()
