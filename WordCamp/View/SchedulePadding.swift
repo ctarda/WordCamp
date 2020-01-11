33e7349124e340400fd0a2d0432502c7f385b38b
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2020-01-11 16:34:01 +0800

    Add a comment

--- WordCamp/View/SchedulePadding.swift
+++ WordCamp/View/SchedulePadding.swift
@@ -1,5 +1,8 @@
 import SwiftUI
 
+/// Custom view modifier that applies the default padding
+/// on iPadOS, so that the Schedule can be presented
+/// as a split view
 struct SchedulePadding: ViewModifier {
     func body(content: Content) -> some View {
         Group {
